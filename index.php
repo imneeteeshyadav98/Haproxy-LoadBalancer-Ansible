c6c90a6c6857108b821e6252c7a90e69201593c4
Neetesh <neeteeshyadav98@gmail.com>
2021-03-23 12:03:05 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webpage</title>
+</head>
+<body>
+<h1>Ip Addresss...............</h1>
+<pre>
+	<?php
+	print `/usr/sbin/ifconfig`;
+	?>
+</pre>
+</body>
+</html>
